b2338c10426e9778164141e1112b530dc4c9ece7
Alister Burt <alisterburt@gmail.com>
2025-06-19 11:25:56 -0700

    update pretrained weights

--- src/torch_segment_membranes_2d/model/_pretrained_weights.py
+++ src/torch_segment_membranes_2d/model/_pretrained_weights.py
@@ -5,12 +5,12 @@ import pooch
 # https://github.com/fatiando/pooch
 GOODBOY = pooch.create(
     path=pooch.os_cache("torch-segment-membranes-2d"),
-    base_url="doi:10.5281/zenodo.15660525",
-    registry={"epoch=4-step=2000.ckpt": "md5:b4455a96b86c07ef5cf9dbc0b08e48c2"},
+    base_url="doi:10.5281/zenodo.15700479",
+    registry={"epoch=12-step=5000.ckpt": "md5:ddcebb73df1270ce432a72779df672df"},
 )
 
 
 def get_latest_checkpoint() -> Path:
     """Retrieve the latest checkpoint from cache if available or download."""
-    checkpoint_file = Path(GOODBOY.fetch("epoch=4-step=2000.ckpt"))
+    checkpoint_file = Path(GOODBOY.fetch("epoch=12-step=5000.ckpt"))
     return checkpoint_file
